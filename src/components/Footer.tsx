3851a5b4a2765aa17d12c2c63291da57d00a6dbd
yossefflix <yossefflix@gmail.com>
2025-12-26 22:41:21 +0200

    Delete src directory

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import { Mail, Phone, MapPin } from "lucide-react";
-const Footer = () => {
-  return <footer className="relative py-16 border-t border-border/50">
-      
-    </footer>;
-};
-export default Footer;
